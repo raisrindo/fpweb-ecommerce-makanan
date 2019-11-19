0860ba065aaf7027e90c7c82b43b99e79cd27731
redhow <redhogithub@gmail.com>
2019-11-19 14:32:06 +0700

    menambahkan fitur about

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -1,1 +1,108 @@
-<h1>Ini About</h1>
+<div class="bg-light py-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">About</strong></div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <div class="block-16">
+              <figure>
+                <img src="images/blog_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="ion-md-play"></span></a>
+
+              </figure>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-5">
+            
+            
+            <div class="site-section-heading pt-3 mb-4">
+              <h2 class="text-black"> About MakananKite </h2>
+            </div>
+            <p> MakananKite merupakan suatu E-commerce yang baru berdiri pada November 2019. E-commerce ini didirikan oleh empat orang Mahasiswa Ilmu Komputer yaitu Achel, Aditya, Rais dan Redho dalam hal memenuhi tugas besar mata kuliah Pemrograman Web.</p>
+            <p> MakananKite menyediakan berbagai macam jenis makanan khas betawi, mulai dari makanan ringan sampai makanan berat. Jadi untuk para pelanggan dari luar jakarta yang ingin menikmati produk-produk dari MakananKite namun tidak sedang berada di Jakarta, Tenang ! MakananKite solusinya !!!</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 site-section-heading text-center pt-4">
+            <h2>THE TEAM</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+  
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_1.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Muhammad Rais Ridho</h3>
+                  <p class="block-38-subheading">Programmer</p>
+                </div>
+                <div class="block-38-body">
+                  <p> BELUM !</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_2.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Tiurmauli Rahel Ernita</h3>
+                  <p class="block-38-subheading">Programmer</p>
+                </div>
+                <div class="block-38-body">
+                  <p> BELUM ! </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_3.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Adhitya Lazuardy</h3>
+                  <p class="block-38-subheading">Programmer</p>
+                </div>
+                <div class="block-38-body">
+                  <p> Adhit adalah salah satu programmer profesional yang saat ini merupakan tenaga developer di MakananKite,
+                      selain itu Adhit juga mempunyai banyak bisnis di bidang lain. beliau merupakan lulusan terbaik di Tokyo University.  
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Redho Saputra</h3>
+                  <p class="block-38-subheading">Programmer</p>
+                </div>
+                <div class="block-38-body">
+                  <p> Redho Saputra adalah seorang Manusia biasa yang saat ini merupakan seorang pengusaha sukses 
+                      yang bergerak dibidang makanan. Redho sekarang sedang berfokus menjalankan bisnis makanannya 
+                      yaitu MakananKite, Beliau merupakan salah satu lulusan dengan predikat terbaik di Universitas Bengkulu.  
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
