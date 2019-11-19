be9808c2941ba6371347290f82a695663fa27421
adhityal127 <adityasuarez7@gmail.com>
2019-11-19 14:49:09 +0700

    merubah div class di shop

--- application/views/home/shop.php
+++ application/views/home/shop.php
@@ -1,7 +1,7 @@
 <div class="bg-light py-3">
       <div class="container">
         <div class="row">
-          <div class="col-md-12 mb-0"><?php base_url();?>Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Shop</strong></div>
+          <div class="col-md-12 mb-0"><a href="<?php base_url();?>home">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Shop</strong></div>
         </div>
       </div>
     </div>
